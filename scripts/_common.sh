8f094fa48eae2d6a8607da297927997fac96c6bd
navanchauhan <navanchauhan@gmail.com>
2022-07-04 17:04:49 +0530

    try2fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
+# dependencies used by the app
+pkg_dependencies="dotnet-sdk-6.0"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
