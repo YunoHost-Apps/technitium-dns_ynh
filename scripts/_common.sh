477405d03e5de275bf631410bff9cd7a0db47fa9
navanchauhan <navanchauhan@gmail.com>
2022-07-04 17:39:22 +0530

    handling installing dotnet

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="aspnetcore-runtime-6.0"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
